a748e00bf1db262936400ee34ff27d70ed41e34d
Forrest Bicker <bickerforrest@gmail.com>
2021-09-24 11:37:24 -0700

    create gyro object to track pos, vel, and accel

---
+++ js/Gyro.js
@@ -0,0 +1,14 @@
+class Gyro {
+    constructor(universe) {
+        this.universe = universe;
+        this.x = 100;
+        this.y = 110;
+        this.theta = 0;
+
+        this.vx = 0;
+        this.vy = 0;
+
+        this.ax = 0;
+        this.ay = 0;
+    }
+}
