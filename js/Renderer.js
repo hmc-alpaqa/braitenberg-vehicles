17002e6dab559d87953eac454a6f0ed468ea74dc
Forrest Bicker <bickerforrest@gmail.com>
2021-09-26 21:46:28 -0700

    export all rendering to Renderer class

---
+++ js/Renderer.js
@@ -0,0 +1,55 @@
+class Renderer {
+    constructor(robot) {
+        this.robot = robot;
+    }
+
+    renderAll() {
+        this.renderRobot();
+        this.renderText();
+    }
+
+    renderRobot() {
+        // noStroke()
+        fill(225, 225, 225);
+        // render the body of the robot so that it is positioned at the center of the gyro
+        square(
+            this.robot.gyro.r.x * PIXEL_SIZE - ROBOT_SIZE / 2, // the - ROBOT_SIZE / 2 is to center the robot in the center of the gyro rather than draw the upper left corner at the center of the gyro
+            this.robot.gyro.r.y * PIXEL_SIZE - ROBOT_SIZE / 2,
+            ROBOT_SIZE
+        );
+        this.renderSensors();
+        this.renderMotors();
+    }
+
+    renderSensors() {
+        for (i = 0; i < this.robot.sensors.length; i++) {
+            fill(0, 225, 0);
+            square(
+                this.robot.sensors[i].getX() * PIXEL_SIZE - SENSOR_SIZE / 2,
+                this.robot.sensors[i].getY() * PIXEL_SIZE - SENSOR_SIZE / 2,
+                SENSOR_SIZE);
+        }
+    }
+
+    renderMotors() {
+        for (i = 0; i < this.robot.motors.length; i++) {
+            fill(0, 0, 225);
+            square(
+                this.robot.motors[i].getX() * PIXEL_SIZE - SENSOR_SIZE / 2,
+                this.robot.motors[i].getY() * PIXEL_SIZE - SENSOR_SIZE / 2,
+                SENSOR_SIZE);
+        }
+    }
+
+    renderText() {
+        text('x: ' + this.robot.gyro.r.x.toFixed(2), 10, 10)
+        text('y: ' + this.robot.gyro.r.y.toFixed(2), 10, 30)
+        text('θ: ' + this.robot.gyro.θ.toFixed(2), 10, 50)
+        text('vx: ' + this.robot.gyro.v.x.toFixed(2), 80, 10)
+        text('vy: ' + this.robot.gyro.v.y.toFixed(2), 80, 30)
+        text('ω: ' + this.robot.gyro.ω.toFixed(2), 80, 50)
+        text('ax: ' + this.robot.gyro.a.x.toFixed(2), 150, 10)
+        text('ay: ' + this.robot.gyro.a.y.toFixed(2), 150, 30)
+        text('α: ' + this.robot.gyro.α.toFixed(2), 150, 50)
+    }
+}
--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ function setup() {
 
         }
     }
-
+    renderer = new Renderer(r);
 }
 
 function draw() {
@@ -34,50 +34,7 @@ function draw() {
 
     image(pg, 0, 0, MAP_SIZE, MAP_SIZE);
 
-    renderRobot(r);
-    renderText(r);
-    r.move(1 / FPS);
-
-    
+    renderer.renderAll()
+    r.step(1 / FPS);
 }
 
-function renderRobot(robot) {
-    // noStroke()
-    fill(225, 225, 225);
-    // render the body of the robot so that it is positioned at the center of the gyro
-    square(
-        robot.gyro.x * PIXEL_SIZE - ROBOT_SIZE / 2, // the - ROBOT_SIZE / 2 is to center the robot in the center of the gyro rather than draw the upper left corner at the center of the gyro
-        robot.gyro.y * PIXEL_SIZE - ROBOT_SIZE / 2,
-        ROBOT_SIZE
-    );
-    renderSensors(robot);
-    renderMotors(robot);
-}
-
-function renderSensors(robot) {
-    for (i = 0; i < robot.sensors.length; i++) {
-        fill(0, 225, 0);
-        square(
-            robot.sensors[i].getX() * PIXEL_SIZE - SENSOR_SIZE / 2,
-            robot.sensors[i].getY() * PIXEL_SIZE - SENSOR_SIZE / 2,
-            SENSOR_SIZE);
-    }
-}
-
-function renderMotors(robot) {
-    for (i = 0; i < robot.motors.length; i++) {
-        fill(0, 0, 225);
-        square(
-            robot.motors[i].getX() * PIXEL_SIZE - SENSOR_SIZE / 2,
-            robot.motors[i].getY() * PIXEL_SIZE - SENSOR_SIZE / 2,
-            SENSOR_SIZE);
-    }
-}
-
-function renderText(robot) {
-    text('x: ' + robot.gyro.x.toFixed(2), 10, 10)
-    text('y: ' + robot.gyro.y.toFixed(2), 10, 30)
-    text('vx: ' + robot.gyro.vx.toFixed(2), 80, 10)
-    text('vy: ' + robot.gyro.vy.toFixed(2), 80, 30)
-
-}
