fd45b0d5f89560539c76e0409a896bb1d98d4eea
Forrest Bicker <bickerforrest@gmail.com>
2021-09-29 16:39:32 -0700

    sad broken vector math

--- js/Components/Component.js
+++ js/Components/Component.js
@@ -8,12 +8,13 @@ class Component {
         let x = this.offset.x
         let y = this.offset.y
         let θ = this.gyro.θ
-        let a = x / Math.tan(this.gyro.θ);
-        let b = y - a;
-        let xEffective = b * Math.sin(θ);
-        let c = b * Math.cos(θ);
-        let d = x / Math.sin(θ);
-        let yEffective = c + d;
+        let a = y / Math.abs(Math.cos(θ));
+        let b = Math.abs(Math.tan(θ)) / y;
+        b = isNaN(b) ? 0 : b;
+        let c = x - b;
+        let d = c * Math.abs(Math.sin(θ));
+        let xEffective = c * Math.abs(Math.cos(θ));
+        let yEffective = a + d;
         return new Vector(
             this.gyro.r.x + xEffective,
             this.gyro.r.y + yEffective,
