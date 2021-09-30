d9e352b5da59ef3c09452f451e6cda3a3a206119
Forrest Bicker <bickerforrest@gmail.com>
2021-09-29 17:10:22 -0700

    fixed rotational coordinate calculation

--- js/Components/Component.js
+++ js/Components/Component.js
@@ -7,14 +7,30 @@ class Component {
     getR() {
         let x = this.offset.x
         let y = this.offset.y
-        let θ = this.gyro.θ
-        let a = y / Math.abs(Math.cos(θ));
-        let b = Math.abs(Math.tan(θ)) / y;
-        b = isNaN(b) ? 0 : b;
-        let c = x - b;
-        let d = c * Math.abs(Math.sin(θ));
-        let xEffective = c * Math.abs(Math.cos(θ));
-        let yEffective = a + d;
+
+        let r = Math.sqrt(x * x + y * y)
+        let θ = 0;
+
+        if (x > 0) {
+            θ = Math.atan2(y, x)
+        } else {
+            θ = Math.PI + Math.atan2(y, x)
+        }
+
+        let rotateBy = this.gyro.θ
+
+        let θEffective = θ + rotateBy
+
+        let xEffective = r * Math.cos(θEffective)
+        let yEffective = r * Math.sin(θEffective)
+
+        // let a = y / Math.abs(Math.cos(θ));
+        // let b = Math.abs(Math.tan(θ)) / y;
+        // b = isNaN(b) ? 0 : b;
+        // let c = x - b;
+        // let d = c * Math.abs(Math.sin(θ));
+        // let xEffective = c * Math.abs(Math.cos(θ));
+        // let yEffective = a + d;
         return new Vector(
             this.gyro.r.x + xEffective,
             this.gyro.r.y + yEffective,
