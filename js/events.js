2dccc94fe15d3155bb8de8f1933c616abddf9961
Forrest Bicker <bickerforrest@gmail.com>
2021-10-06 10:55:49 -0700

    add a slider for input to friction magnitude

--- js/events.js
+++ js/events.js
@@ -1,4 +1,15 @@
 document.getElementById("add-vehicle").addEventListener("click", () => {
     addingRobot = true;
-    console.log(addingRobot);
+    // console.log(addingRobot);
 })
+
+function updateFriction(value) {
+    friction = parseInt(value);
+}
+// document.getElementById("friction-slider").addEventListener("oninput", () => {
+//     friction = document.getElementById("friction-slider").value;
+// })
+
+// document.getElementById("friction-slider").addEventListener("onchange", () => {
+//     friction = document.getElementById("friction-slider").value;
+// })
