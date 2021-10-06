2d46f393dd4434a4929ba1dd76940a61d3d7d1d6
Forrest Bicker <bickerforrest@gmail.com>
2021-10-06 10:55:15 -0700

    add mangintude and theta getters for vector object

--- js/Vector.js
+++ js/Vector.js
@@ -3,4 +3,16 @@ class Vector {
         this.x = x;
         this.y = y;
     }
+
+    getMagnitude() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    getTheta() {
+        if (Math.sign(x) == Math.sign(y)) {
+            return Math.atan2(x, y)
+        } else {
+            return Math.PI + Math.atan2(x, y)
+        }
+    }
 }
