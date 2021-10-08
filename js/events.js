a9349e7542a2c49557f3b098ddf432027b47014a
Vivian Tao <vivian0tao@gmail.com>
2021-10-08 16:25:22 -0700

    made buttons responsive

--- js/events.js
+++ js/events.js
@@ -5,7 +5,15 @@ document.querySelector("#add-vehicle-button").addEventListener("click", () => {
 let simulationPaused = true;
 let startStopButton = document.querySelector("#start-stop-button");
 startStopButton.addEventListener("click", () => {
-    startStopButton.innerText = simulationPaused ? "Stop" : "Start";
+    if (simulationPaused) {
+        startStopButton.innerText = "Stop";
+        startStopButton.style.color = "#FFFFFF";
+        startStopButton.style.backgroundColor = "#000000";
+    } else {
+        startStopButton.innerText = "Start";
+        startStopButton.style.backgroundColor = "#EAAA00";
+        startStopButton.style.color = "black";
+    }
     simulationPaused = !simulationPaused;
 })
 
