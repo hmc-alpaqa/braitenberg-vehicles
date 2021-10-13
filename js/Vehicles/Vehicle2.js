b1c8b452651590300bf99705b5aa6fa2d0adb808
Vivian Tao <vivian0tao@gmail.com>
2021-10-13 08:40:24 -0700

    renamed Coward to Vehicle2a and Aggressive to Vehicle2b

--- js/Vehicles/Vehicle2.js
+++ js/Vehicles/Vehicle2.js
@@ -1,15 +1,17 @@
-function Coward(u, x, y) {
+////////// COWARD //////////
+function Vehicle2a(u, x, y) {
     let gyro = new Gyro(u, x, y);
     let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
     let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
-    // wires sensor to opposite motor
+    // wires sensor to same motor
     let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor1]);
     let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor2]);
     let vehicle = new Vehicle(gyro, [sensor1, sensor2], [], [motor1, motor2]);
     return vehicle;
 }
 
-function Aggressive(u, x, y) {
+////////// AGGRESSIVE //////////
+function Vehicle2b(u, x, y) {
     let gyro = new Gyro(u, x, y);
     let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
     let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
