31c8bfc14d84142d0a97d846419a486e2c11db06
Forrest Bicker <bickerforrest@gmail.com>
2021-11-10 15:32:28 -0800

    comments

--- js/Vehicles/Vehicle2.js
+++ js/Vehicles/Vehicle2.js
@@ -3,9 +3,9 @@ function Vehicle2a(u, x, y) {
     let gyro = new Gyro(u, x, y, θ);
     let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
     let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
-    // wires sensor to same motor
-    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor1]);
-    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor2]);
+    // wires sensor to opposite motor
+    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor2]);
+    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor1]);
     let vehicle = new Vehicle(gyro, [sensor1, sensor2], [], [motor1, motor2]);
     return vehicle;
 }
@@ -15,9 +15,9 @@ function Vehicle2b(u, x, y) {
     let gyro = new Gyro(u, x, y, θ);
     let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
     let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
-    // wires sensor to opposite motor
-    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor2]);
-    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor1]);
+    // wires sensor to same motor
+    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor1]);
+    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor2]);
     let vehicle = new Vehicle(gyro, [sensor1, sensor2], [], [motor1, motor2]);
     return vehicle;
 }
