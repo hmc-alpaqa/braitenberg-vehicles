d0936f29666736756721171e149cd8259f81665c
Forrest Bicker <bickerforrest@gmail.com>
2021-12-01 15:50:05 -0800

    Update Vehicle2.js

--- js/Vehicles/Vehicle2.js
+++ js/Vehicles/Vehicle2.js
@@ -1,11 +1,11 @@
 ////////// COWARD //////////
 function Vehicle2a(u, x, y) {
     let gyro = new Gyro(u, x, y, θ);
-    let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
-    let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
+    let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
+    let sensor2 = new Sensor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE));
     // wires sensor to opposite motor
-    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor2]);
-    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor1]);
+    let motor1 = new Motor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor2]);
+    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor1]);
     let vehicle = new Vehicle(gyro, [sensor1, sensor2], [], [motor1, motor2]);
     return vehicle;
 }
@@ -13,11 +13,11 @@ function Vehicle2a(u, x, y) {
 ////////// AGGRESSIVE //////////
 function Vehicle2b(u, x, y) {
     let gyro = new Gyro(u, x, y, θ);
-    let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
-    let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
+    let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
+    let sensor2 = new Sensor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE));
     // wires sensor to same motor
-    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor1]);
-    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor2]);
+    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor2]);
+    let motor2 = new Motor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor1]);
     let vehicle = new Vehicle(gyro, [sensor1, sensor2], [], [motor1, motor2]);
     return vehicle;
 }
