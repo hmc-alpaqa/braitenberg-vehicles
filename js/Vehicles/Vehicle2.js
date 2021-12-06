10e2df80f7fddf072a50f3ec59e3dd0a71383163
Vivian Tao <vivian0tao@gmail.com>
2021-12-05 21:29:53 -0800

    fixed vehicle 1 and changed vehicle offsets to be constants

--- js/Vehicles/Vehicle2.js
+++ js/Vehicles/Vehicle2.js
@@ -1,11 +1,11 @@
 ////////// COWARD //////////
 function Vehicle2a(u, x, y) {
     let gyro = new Gyro(u, x, y, θ, Vehicles.VEHICLE2A);
-    let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
-    let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
+    let sensor1 = new Sensor(gyro, new Vector(1, -2));
+    let sensor2 = new Sensor(gyro, new Vector(1, 2));
     // wires sensor to opposite motor
-    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor2]);
-    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor1]);
+    let motor1 = new Motor(gyro, new Vector(-1, -2), [sensor2]);
+    let motor2 = new Motor(gyro, new Vector(-1, 2), [sensor1]);
     let vehicle = new Vehicle(gyro, [sensor1, sensor2], [], [motor1, motor2]);
     return vehicle;
 }
@@ -13,11 +13,11 @@ function Vehicle2a(u, x, y) {
 ////////// AGGRESSIVE //////////
 function Vehicle2b(u, x, y) {
     let gyro = new Gyro(u, x, y, θ, Vehicles.VEHICLE2B);
-    let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
-    let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
+    let sensor1 = new Sensor(gyro, new Vector(1, -2));
+    let sensor2 = new Sensor(gyro, new Vector(1, 2));
     // wires sensor to same motor
-    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor1]);
-    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor2]);
+    let motor1 = new Motor(gyro, new Vector(-1, -2), [sensor1]);
+    let motor2 = new Motor(gyro, new Vector(-1, 2), [sensor2]);
     let vehicle = new Vehicle(gyro, [sensor1, sensor2], [], [motor1, motor2]);
     return vehicle;
 }
@@ -25,11 +25,11 @@ function Vehicle2b(u, x, y) {
 ////////// LUXURIOUS VEHICLE 1 //////////
 function Vehicle2c(u, x, y) {
     let gyro = new Gyro(u, x, y, θ, Vehicles.VEHICLE2C);
-    let sensor1 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, -PIXEL_SIZE));
-    let sensor2 = new Sensor(gyro, new Vector(PIXEL_SIZE / 2, PIXEL_SIZE));
+    let sensor1 = new Sensor(gyro, new Vector(1, -2));
+    let sensor2 = new Sensor(gyro, new Vector(1, 2));
     // wires sensors to both motors
-    let motor1 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, -PIXEL_SIZE), [sensor1, sensor2]);
-    let motor2 = new Motor(gyro, new Vector(-PIXEL_SIZE / 2, PIXEL_SIZE), [sensor1, sensor2]);
+    let motor1 = new Motor(gyro, new Vector(-1, -2), [sensor1, sensor2]);
+    let motor2 = new Motor(gyro, new Vector(-1, 2), [sensor1, sensor2]);
     let vehicle = new Vehicle(gyro, [sensor1, sensor2], [], [motor1, motor2]);
     return vehicle;
 }
