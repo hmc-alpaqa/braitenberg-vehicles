a64eabfa97febe8822098cc47c4d5bb122e0e65b
v-tao <vivian0tao@gmail.com>
2024-05-06 19:37:48 -0700

    turn vehicle3 into subclasses

--- js/Vehicles/Vehicle3.js
+++ js/Vehicles/Vehicle3.js
@@ -1,21 +1,23 @@
 ////////// LOVE //////////
-function Vehicle3a(gyro, id) {
-    let sensor1 = new Sensor(gyro, new Vector(1, -2));
-    let sensor2 = new Sensor(gyro, new Vector(1, 2));
-    // wires sensor to same inhibitor
-    let inhibitor1 = new Inhibitor(gyro, new Vector(-1, -2), [sensor2], -1, vehicle3StartingVelocity);
-    let inhibitor2 = new Inhibitor(gyro, new Vector(-1, 2), [sensor1], -1, vehicle3StartingVelocity);
-    let vehicle = new Vehicle(gyro, Vehicles.VEHICLE3A, id, [sensor1, sensor2], [], [inhibitor1, inhibitor2]);
-    return vehicle;
+class Vehicle3a extends Vehicle {
+    constructor(gyro, id) {
+        let sensor1 = new Sensor(gyro, new Vector(1, -2));
+        let sensor2 = new Sensor(gyro, new Vector(1, 2));
+        // wires sensor to same inhibitor
+        let inhibitor1 = new Inhibitor(gyro, new Vector(-1, -2), [sensor2], -1, vehicle3StartingVelocity);
+        let inhibitor2 = new Inhibitor(gyro, new Vector(-1, 2), [sensor1], -1, vehicle3StartingVelocity);
+        super(gyro, Vehicles.VEHICLE3A, id, [sensor1, sensor2], [inhibitor1, inhibitor2]);
+    }
 }
 
 ////////// EXPLORER //////////
-function Vehicle3b(gyro) {
-    let sensor1 = new Sensor(gyro, new Vector(1, -2));
-    let sensor2 = new Sensor(gyro, new Vector(1, 2));
-    // wires sensor to same inhibitor
-    let inhibitor1 = new Inhibitor(gyro, new Vector(-1, -2), [sensor1], -1, vehicle3StartingVelocity);
-    let inhibitor2 = new Inhibitor(gyro, new Vector(-1, 2), [sensor2], -1, vehicle3StartingVelocity);
-    let vehicle = new Vehicle(gyro, Vehicles.VEHICLE3B, id, [sensor1, sensor2], [], [inhibitor1, inhibitor2]);
-    return vehicle;
+class Vehicle3b extends Vehicle {
+    constructor(gyro, id) {
+        let sensor1 = new Sensor(gyro, new Vector(1, -2));
+        let sensor2 = new Sensor(gyro, new Vector(1, 2));
+        // wires sensor to same inhibitor
+        let inhibitor1 = new Inhibitor(gyro, new Vector(-1, -2), [sensor1], -1, vehicle3StartingVelocity);
+        let inhibitor2 = new Inhibitor(gyro, new Vector(-1, 2), [sensor2], -1, vehicle3StartingVelocity);
+        super(gyro, Vehicles.VEHICLE3B, id, [sensor1, sensor2], [inhibitor1, inhibitor2]);
+    }
 }
