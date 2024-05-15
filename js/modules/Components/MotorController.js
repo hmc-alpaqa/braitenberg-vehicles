baa525fc8c307223ed283e48e4539e267642bbf0
v-tao <vivian0tao@gmail.com>
2024-05-15 06:46:11 -0400

    add motor controller docs

--- js/modules/Components/MotorController.js
+++ js/modules/Components/MotorController.js
@@ -1,10 +1,24 @@
+/**
+ * Class representing a motor controller. Modifies the input by some function.
+ */
 class MotorController extends Component {
+    /**
+     * 
+     * @param {Gyro} gyro gyro belonging to the vehicle the inhibitor is on
+     * @param {Vector} offset position of the motor controller relative to the center of the vehicle
+     * @param {Component[]} inputs inputs to the motor controller
+     * @param {*} f function that modifies the motor controller input
+     */
     constructor(gyro, offset, inputs = [], f) {
         super(gyro, offset);
         this.inputs = inputs;
         this.f = f;
     }
 
+    /**
+     * Returns the motor controller output
+     * @returns {Number} motor controller output
+     */
     getOutput() {
         let sum = 0
         for (let input of this.inputs) {
