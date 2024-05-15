2e07c23b46deaa8607e34d4cddc1f2286603c87f
v-tao <vivian0tao@gmail.com>
2024-05-15 06:48:39 -0400

    add sensor docs

--- js/modules/Components/Sensor.js
+++ js/modules/Components/Sensor.js
@@ -1,9 +1,21 @@
+/**
+ * Class representing a sensor. Detects stimuli from the sources.
+ * @extends Component
+ */
 class Sensor extends Component {
+    /**
+     * 
+     * @param {Gyro} gyro gyro belonging to the vehicle the sensor is on
+     * @param {Number} offset position of the sensor relative to the center of the vehicle
+     */
     constructor(gyro, offset) {
         super(gyro, offset);
-        this.theta = 0; // orientation of sensor
     }
 
+    /**
+     * Returns the stimulus sensed at the sensor
+     * @returns {Number} sensor output
+     */
     getOutput() { 
         let y = Math.round(this.getR().y);
         let x = Math.round(this.getR().x);
